63cfed234a7cbd9ab7e8885533a4fe9795edb80c
Luis Mendoza <luismendoza2295@gmail.com>
2020-12-22 18:44:43 -0500

    init script fix

--- init.sql
+++ init.sql
@@ -8,5 +8,5 @@ CREATE TABLE products
 
 INSERT INTO products(code, name, price) VALUES ('P001', 'M&M', 2.5);
 INSERT INTO products(code, name, price) VALUES ('P002', 'Lays Small', 0.75);
-INSERT INTO products(code, name, price) VALUES ('P002', 'Lays Medium', 1.75);
+INSERT INTO products(code, name, price) VALUES ('P003', 'Lays Medium', 1.75);
 INSERT INTO products(code, name, price) VALUES ('P004', 'Lays Large', 2.25);
